64b576d1bb1a04f925d2fd2e230d64fa3b1005b2
breonh <breonh@uw.edu>
2021-10-20 12:43:54 -0700

    updates ahead of week 4 lecture

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -2,3 +2,7 @@
 
 
 pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week3/csss_508_week3_dplyr.html", format="pdf")
+
+
+
+knitr::purl("csss_508_week4_data_structures.Rmd", output = "csss_508_week4_rscript_companion.R")
