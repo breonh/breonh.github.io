dd5d549c76a0dbbd4c713c36d4a59cb0b390bb4e
breonh <breonh@uw.edu>
2021-10-27 12:57:43 -0700

    double checking if the data is up

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -1,8 +1,8 @@
 # Rendering The Slides as PDFs
 
 
-pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week4/csss_508_week4_data_structures.html", format="pdf")
+pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week5/csss_508_week5_import_export.html", format="pdf")
 
 
 
-knitr::purl("csss_508_week4_data_structures.Rmd", output = "csss_508_week4_rscript_companion.R")
+knitr::purl("csss_508_week5_import_export.Rmd", output = "csss_508_week5_rscript_companion.R")
