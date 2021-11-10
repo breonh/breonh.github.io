ee5378041b37f75038ef17cadf8969e84fb362c0
breonh <breonh@uw.edu>
2021-11-10 13:16:28 -0800

    uploading the pdf for week 7

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -1,8 +1,8 @@
 # Rendering The Slides as PDFs
 
 
-pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week6/csss_508_week6_loops.html", format="pdf")
+pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week7/csss_508_week7_vectorization.html", format="pdf")
 
 
 
-knitr::purl("week6/csss_508_week6_loops.Rmd", output = "csss_508_week6_rscript_companion.R")
+knitr::purl("week7/csss_508_week7_vectorization.Rmd", output = "csss_508_week7_rscript_companion.R")
