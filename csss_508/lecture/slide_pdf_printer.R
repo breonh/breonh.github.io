7d90c5d4c55b39e28e2d42b8829a603267566322
breonh <breonh@uw.edu>
2021-12-08 12:58:04 -0800

    getting the week 10 pdfs up there

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -1,7 +1,7 @@
 # Rendering The Slides as PDFs
 
 
-pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week9/CSSS508_Week9_mapping.html#1", format="pdf")
+pagedown::chrome_print("https://breonh.github.io/csss_508/lecture/week10/csss_508_Week10_model_results.html#1", format="pdf")
 
 
 
