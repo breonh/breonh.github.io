e12d6a32b37dc0d948110302f6a5c3b9c84dffa3
breonh <breonh@uw.edu>
2022-03-28 16:37:54 -0700

    updating for sp 2022

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -1,8 +1,6 @@
 # Rendering The Slides as PDFs
 
 
-pagedown::chrome_print("C:/Users/breha/OneDrive/Documents/breonh.github.io/pols_249/Week 1 - Jan 7/pols249_quiz_week1.html", format="pdf")
-
-
+pagedown::chrome_print("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/syllabus.html", format="pdf")
 
 knitr::purl("week10/csss_508_week10_model_results.Rmd", output = "csss_508_week10_rscript_companion.R")
