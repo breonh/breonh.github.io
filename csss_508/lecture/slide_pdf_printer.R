1b9b5892a33b757f747d8b01354fbe51482695b4
breonh <breonh@uw.edu>
2022-03-31 10:11:08 -0700

    a couple typos on the first day sp22

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -1,6 +1,6 @@
 # Rendering The Slides as PDFs
 
 
-pagedown::chrome_print("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/syllabus.html", format="pdf")
+pagedown::chrome_print("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/lecture/week1/csss_508_week1_Rstudio_and_Rmarkdown.html", format="pdf")
 
 knitr::purl("week10/csss_508_week10_model_results.Rmd", output = "csss_508_week10_rscript_companion.R")
