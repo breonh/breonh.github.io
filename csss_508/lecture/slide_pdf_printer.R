d0fb6a5e61533c5d7167fbc256f8e000b0a8171f
breonh <breonh@uw.edu>
2022-04-21 11:48:40 -0700

    week 4 lecture update

--- csss_508/lecture/slide_pdf_printer.R
+++ csss_508/lecture/slide_pdf_printer.R
@@ -1,6 +1,6 @@
 # Rendering The Slides as PDFs
 
 
-pagedown::chrome_print("C:/Users/Breon/Documents/GitHub/breonh.github.io/csss_508/lecture/week3/csss_508_week3_dplyr.html", format="pdf")
+pagedown::chrome_print("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/lecture/week4/csss_508_week4_data_structures.html", format="pdf")
 
-knitr::purl("C:/Users/Breon/Documents/GitHub/breonh.github.io/csss_508/lecture/week3/csss_508_week3_dplyr.Rmd", output = "csss_508_week3_rscript_companion.R")
+knitr::purl("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/lecture/week4/csss_508_week4_data_structures.Rmd", output = "csss_508_week4_rscript_companion.R")
