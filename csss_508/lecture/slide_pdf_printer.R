61d5784203c0f67b9eab6def172b659f552407d8
breonh <breonh@uw.edu>
2022-05-12 12:57:41 -0700

    week 7 lecture update

--- csss_508/lecture/slide_pdf_printer.R
+++
@@ -1,6 +0,0 @@
-# Rendering The Slides as PDFs
-
-
-pagedown::chrome_print("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/lecture/week5/csss_508_week4_data_structures.html", format="pdf")
-
-knitr::purl("C:/Users/breha/OneDrive/Documents/breonh.github.io/csss_508/lecture/week4/csss_508_week4_data_structures.Rmd", output = "csss_508_week4_rscript_companion.R")
---
+++ csss_508/slide_pdf_printer.R
@@ -0,0 +1,18 @@
+# Rendering The Slides as PDFs
+
+render_and_print_slides <- function(week){
+  week_dir     <- paste0(getwd(), "/lecture/", "week", week, "/")
+  current_rmd  <- paste0(week_dir, stringr::str_subset(list.files(week_dir),
+                                                       "^csss_508_week.*\\.Rmd$"))
+  rmarkdown::render(current_rmd, encoding = "UTF-8")
+  current_html <- stringr::str_replace(current_rmd,  "\\.Rmd",  "\\.html")
+  new_pdf_file <- stringr::str_replace(current_html, "\\.html", "\\.pdf")
+  new_r_script <- stringr::str_replace(current_html, "\\.html", "_companion\\.R")
+  message("Slides rendered, waiting 5 seconds.")
+  Sys.sleep(5)
+  message("Purling slides.")
+  knitr::purl(input = current_rmd, output = new_r_script, documentation = 0)
+  message("Printing from Chrome.")
+  pagedown::chrome_print(current_html, format="pdf")
+  message(paste0("Printing complete at ", week_dir)) }
+
