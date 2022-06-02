22b65bb9667ccf354b32229a0cce02ca1b2ac3bc
breonh <breonh@uw.edu>
2022-06-02 13:11:03 -0700

    week 10 upload

--- csss_508/lecture/week10/csss_508_week10_model_results_companion.R
+++ csss_508/lecture/week10/csss_508_week10_model_results_companion.R
@@ -1,4 +1,3 @@
-## ---- include=FALSE-------------------------------------------------------------------
 library(tidyverse)
 library(pander)
 library(knitr)
@@ -26,74 +25,46 @@ ex_dat <- data.frame(num1 = rnorm(200, 1, 2),
   mutate(fac1 = factor(fac1, labels=c("A", "B", "C")),
          fac2 = factor(fac2, labels=c("Yes", "No")))
 
-
-## ---- warning=FALSE-------------------------------------------------------------------
 library(broom)
 
-
-## -------------------------------------------------------------------------------------
 lm_1 <- lm(yn ~ num1 + fac1, data = ex_dat)
 summary(lm_1)
 
-
-## ---- op------------------------------------------------------------------------------
 glm_1 <- glm(yb ~ num1 + fac1, data = ex_dat, family=binomial(link="logit"))
 summary(glm_1)
 
-
-## -------------------------------------------------------------------------------------
 lm_1 %>% tidy()
 
-
-## -------------------------------------------------------------------------------------
 glm_1 %>% tidy()
 
-
-## -------------------------------------------------------------------------------------
 glance(lm_1)
 
-
-## -------------------------------------------------------------------------------------
 augment(lm_1) %>% head()
 
-
-## -------------------------------------------------------------------------------------
 ex_dat %>% 
   nest_by(fac1) %>% #<<
   mutate(model = list(lm(yn ~  num1 + fac2, data = data))) %>%  
   summarize(tidy(model), .groups = "drop")
 
-
-## ---- warning=FALSE-------------------------------------------------------------------
 library(gapminder)
 
-
-## ---- fig.height = 2.75, fig.width = 7, dev="svg", warning=FALSE, message=FALSE-------
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp, color = continent)) +
   geom_point(position = position_jitter(1,0), size = 0.5) +
   geom_smooth()
 
-
-## ---- fig.height = 2.75, fig.width = 7, dev="svg", warning=FALSE, message=FALSE-------
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp, color = continent)) +
   geom_point(position = position_jitter(1,0), size = 0.5) +
   geom_smooth(method = "glm", formula = y ~ x) #<<
 
-
-## ---- fig.height = 2.75, fig.width = 7, dev="svg", warning=FALSE, message=FALSE-------
 ggplot(data = gapminder, 
        aes(x = year, y = lifeExp, color = continent)) +
   geom_point(position = position_jitter(1,0), size = 0.5) +
   geom_smooth(method = "glm", formula = y ~ poly(x, 2)) #<<
 
-
-## ---- warning=FALSE-------------------------------------------------------------------
 library(ggeffects)
 
-
-## -------------------------------------------------------------------------------------
 ex_dat <- data.frame(num1 = rnorm(200, 1, 2), 
                      fac1 = sample(c(1, 2, 3), 200, TRUE),
                      num2 = rnorm(200, 0, 3),
@@ -104,62 +75,36 @@ ex_dat <- data.frame(num1 = rnorm(200, 1, 2),
   mutate(fac1 = factor(fac1, labels = c("A", "B", "C")),
          fac2 = factor(fac2, labels = c("Yes", "No")))
 
-
-## -------------------------------------------------------------------------------------
 lm_1 <- lm(yn ~ num1 + fac1, data = ex_dat)
 lm_1_est <- ggpredict(lm_1, terms = "num1")
 
-
-## -------------------------------------------------------------------------------------
 lm_1_est
 
-
-## ---- warning=FALSE, message=FALSE, dev="svg", fig.height=4---------------------------
 plot(lm_1_est)
 
-
-## ---- warning=FALSE, message=FALSE, dev="svg", fig.height=3.5-------------------------
 glm(yb ~ num1 + fac1 + num2 + fac2, data = ex_dat, family=binomial(link = "logit")) %>%
   ggpredict(terms = c("num1", "fac1")) %>% plot()
 
-
-## ---- warning=FALSE, message=FALSE, dev="svg", fig.height=3.5-------------------------
 glm(yb ~ num1 + fac1 + num2 + fac2, data = ex_dat, family = binomial(link = "logit")) %>%
   ggpredict(terms = c("num1", "fac1")) %>% plot(facet=TRUE)
 
-
-## ---- warning=FALSE, message=FALSE, dev="svg", fig.height=3.5-------------------------
 glm(yb ~ num1 + fac1 + num2 + fac2, data=ex_dat, family=binomial(link="logit")) %>%
   ggpredict(terms = c("num1 [-1,0,1]", "fac1 [A,B]")) %>% plot(facet=TRUE)
 
-
-## ---- warning=FALSE, message=FALSE, dev="svg", fig.height=3.5-------------------------
 glm(yb ~ num1 + fac1 + num2 + fac2, data = ex_dat, family = binomial(link = "logit")) %>%
   ggpredict(terms = c("num1 [meansd]", "num2 [minmax]")) %>% plot(facet=TRUE)
 
-
-## ---- warning=FALSE, message=FALSE, dev="svg", fig.height=3.5-------------------------
 lm(yn ~ fac1 + fac2, data = ex_dat) %>% 
   ggpredict(terms=c("fac1", "fac2")) %>% plot()
 
-
-## ----pander, warning=FALSE------------------------------------------------------------
 library(pander)
 
-
-## ---- include=FALSE-------------------------------------------------------------------
 panderOptions("table.style", "rmarkdown")
 
-
-## ---- echo=TRUE, eval=FALSE-----------------------------------------------------------
 ## pander(lm_1)
 
-
-## ---- eval=FALSE, echo=TRUE-----------------------------------------------------------
 ## pander(summary(lm_1))
 
-
-## ---- warning=FALSE-------------------------------------------------------------------
 library(gt)
 tes_chars <- starwars %>% 
   unnest(films) %>% 
@@ -171,46 +116,32 @@ tes_chars <- starwars %>%
                             "No Ship", starships))
 glimpse(tes_chars)
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   gt()
 ## 
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>%
   gt() %>% 
   gtsave("img/sw_table_1.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt()
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt() %>%  
   gtsave("img/sw_table_2.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name")
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
   gtsave("img/sw_table_3.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name") %>%
@@ -219,8 +150,6 @@ tes_chars %>%
 ##     subtitle = "The Empire Strikes Back"
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -229,8 +158,6 @@ tes_chars %>%
     subtitle = "The Empire Strikes Back"
   ) %>% gtsave("img/sw_table_4.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name") %>%
@@ -243,8 +170,6 @@ tes_chars %>%
 ##     columns = c(mass, height)
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -257,8 +182,6 @@ tes_chars %>%
     columns = c(mass, height)
   ) %>% gtsave("img/sw_table_5.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name") %>%
@@ -276,8 +199,6 @@ tes_chars %>%
 ##     species = "Species"
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -295,8 +216,6 @@ tes_chars %>%
     species = "Species"
   ) %>% gtsave("img/sw_table_6.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name") %>%
@@ -317,8 +236,6 @@ tes_chars %>%
 ##     columns = c(mass),
 ##     decimals = 0)
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -340,8 +257,6 @@ tes_chars %>%
     decimals = 0
   ) %>% gtsave("img/sw_table_7.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name") %>%
@@ -367,8 +282,6 @@ tes_chars %>%
 ##     columns = c(species, mass, height)
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -394,8 +307,6 @@ tes_chars %>%
     columns = c(species, mass, height)
   ) %>% gtsave("img/sw_table_8.png")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## tes_chars %>%
 ##   group_by(starships) %>%
 ##   gt(rowname_col = "name") %>%
@@ -425,8 +336,6 @@ tes_chars %>%
 ##                "Millennium Falcon")
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -456,14 +365,10 @@ tes_chars %>%
                "Millennium Falcon")
   ) %>% gtsave("img/sw_table_9.png")
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>%
   gt() %>% 
   gtsave("img/sw_table_1.png")
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 tes_chars %>% 
   group_by(starships) %>%
   gt(rowname_col = "name") %>%
@@ -493,22 +398,14 @@ tes_chars %>%
                "Millennium Falcon")
   ) %>% gtsave("img/sw_table_9.png")
 
-
-## ---- message=FALSE, warning=FALSE----------------------------------------------------
 library(modelsummary)
 
-
-## ---- eval=FALSE, echo=TRUE-----------------------------------------------------------
 ## mod_1 <- lm(mpg ~ wt, data = mtcars)
-## msummary(mod_1)
+## modelsummary(mod_1)
 
-
-## ---- eval=TRUE, echo=FALSE-----------------------------------------------------------
 mod_1 <- lm(mpg ~ wt, data = mtcars)
-msummary(mod_1)
+modelsummary(mod_1)
 
-
-## ---- eval=FALSE, echo=TRUE-----------------------------------------------------------
 ## mod_1 <- lm(mpg ~ wt, data = mtcars)
 ## mod_2 <- lm(mpg ~ hp + wt, data = mtcars)
 ## mod_3 <- lm(mpg ~ hp + wt + factor(am),
@@ -516,10 +413,8 @@ msummary(mod_1)
 ## model_list <- list("Model 1" = mod_1,
 ##                    "Model 2" = mod_2,
 ##                    "Model 3" = mod_3)
-## msummary(model_list)
+## modelsummary(model_list)
 
-
-## ---- eval=TRUE, echo=FALSE-----------------------------------------------------------
 mod_1 <- lm(mpg ~ wt, data = mtcars)
 mod_2 <- lm(mpg ~ hp + wt, data = mtcars)
 mod_3 <- lm(mpg ~ hp + wt + factor(am), 
@@ -527,26 +422,18 @@ mod_3 <- lm(mpg ~ hp + wt + factor(am),
 model_list <- list("Model 1" = mod_1, 
                    "Model 2" = mod_2, 
                    "Model 3" = mod_3)
-msummary(model_list, output = "gt")
+modelsummary(model_list, output = "gt")
 
+## modelsummary(model_list, output = "latex")
 
-## ---- eval=FALSE----------------------------------------------------------------------
-## msummary(model_list, output = "latex")
+## modelsummary(model_list, output = "ex_table.png")
 
-
-## -------------------------------------------------------------------------------------
-msummary(model_list, output = "ex_table.png")
-
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## msummary(model_list, output = "gt") %>%
 ##   tab_header(
 ##     title = "Table 1. Linear Models",
 ##     subtitle = "DV: Miles per Gallon"
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 msummary(model_list, output = "gt") %>%
   tab_header(
     title = "Table 1. Linear Models", 
@@ -554,36 +441,24 @@ msummary(model_list, output = "gt") %>%
   ) %>%
   gtsave("img/gt_summary.png")
 
-
-## ---- warning=FALSE-------------------------------------------------------------------
 library(gtsummary)
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## mtcars %>%
 ##   select(1:9) %>%
 ##   tbl_summary()
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 mtcars %>% 
   select(1:9) %>%
   tbl_summary()
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## mtcars %>%
 ##   select(1:9) %>%
 ##   tbl_summary(by = "am")
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 mtcars %>% 
   select(1:9) %>%
   tbl_summary(by = "am")
 
-
-## ---- eval=FALSE----------------------------------------------------------------------
 ## mtcars %>%
 ##   select(1:9) %>%
 ##   tbl_summary(by = "am") %>%
@@ -597,8 +472,6 @@ mtcars %>%
 ##     subtitle = "Descriptive Statistics"
 ##   )
 
-
-## ---- echo=FALSE----------------------------------------------------------------------
 mtcars %>% 
   select(1:9) %>%
   tbl_summary(by = "am") %>%
@@ -609,8 +482,6 @@ mtcars %>%
              subtitle = "Descriptive Statistics") %>%
   gtsave("img/gtsummary.png")
 
-
-## ---- message=FALSE, warning=FALSE, eval=FALSE----------------------------------------
 ## library(corrplot)
 ## corrplot(
 ##   cor(mtcars),
@@ -619,8 +490,5 @@ mtcars %>%
 ##   type="upper",
 ##   order="AOE")
 
-
-## ---- message=FALSE, warning=FALSE, eval=TRUE, echo=FALSE, dev="svg", fig.height=5----
 library(corrplot)
 corrplot(cor(mtcars), addCoef.col = "white", addCoefasPercent=T, type="upper", order="FPC")
-
